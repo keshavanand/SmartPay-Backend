38eab1e2a9cab5ab9e31f7a1a181370d9d916019
keshavanand <keshavanandsingh89@gmail.com>
2026-02-07 09:57:11 -0500

    boilerplate

---
+++ src/main/java/com/keshav/smartPay/SmartPayApplication.java
@@ -0,0 +1,13 @@
+package com.keshav.smartPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPayApplication.class, args);
+	}
+
+}
